fd4e2742115dca172cb0f9c4b2b654e6c9c4ebef
Carlos Silva <csilva2810@gmail.com>
2017-12-19 16:47:13 -0200

    Added fizzBuzz lib with tests

---
+++ src/fizzbuzz.js
@@ -0,0 +1,9 @@
+function fizzBuzz(number) {
+  if (number % 3 === 0 && number % 5 === 0) return 'Fizz Buzz';
+  if (number % 5 === 0) return 'Buzz';
+  if (number % 3 === 0) return 'Fizz';
+
+  return number;
+}
+
+export default fizzBuzz;
